3fb9a62cb757fa19338c23029c6e2b404b50454a
Jim Liu <1234959@qq.com>
2018-11-21 23:04:06 +0800

    change timer correct

--- main.ts
+++ main.ts
@@ -189,7 +189,7 @@ namespace powerfunctions {
      * The motor's power is switched off and thus the motor will roll to a stop.
      */
     //% blockId=pf_float
-    //% block="ffff2 | motor %motor | to stop"
+    //% block="ffff3 | motor %motor | to stop"
     //% weight=70
     //% motor.fieldEditor="gridpicker" motor.fieldOptions.columns=4 motor.fieldOptions.tooltips="false"
     export function float(motor: PowerFunctionsMotor) {
@@ -262,10 +262,10 @@ namespace powerfunctions {
 
     namespace transport {
 
-        const IR_MARK = 6 * 1000000 / 38000
-        const START_STOP_PAUSE = 39 * 1000000 / 38000
-        const LOW_PAUSE = 10 * 1000000 / 38000
-        const HIGH_PAUSE = 21 * 1000000 / 38000
+        const IR_MARK = 11.84 * 1000000 / 38000
+        const START_STOP_PAUSE = 11.84 * 1000000 / 38000
+        const LOW_PAUSE = 4.21 * 1000000 / 38000
+        const HIGH_PAUSE = 7.11 * 1000000 / 38000
 
         export class InfraredDevice {
             private pin: AnalogPin
@@ -280,7 +280,7 @@ namespace powerfunctions {
             }
 
             transmitBit(markMicroSeconds: number, pauseMicroSeconds: number): void {
-                pins.analogWritePin(this.pin, 210)
+                pins.analogWritePin(this.pin, 511)
                 control.waitMicros(Math.max(1, markMicroSeconds + markTimingCorrectionMicroSeconds))
                 pins.analogWritePin(this.pin, 0)
                 control.waitMicros(Math.max(1, pauseMicroSeconds + pauseTimingCorrectionMicroSeconds))
