4815ace9330fd43e086d166f0bbda9a161c30b91
Jim Liu <1234959@qq.com>
2018-11-22 23:06:57 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -283,9 +283,9 @@ namespace powerfunctions {
 
             transmitBit(markMicroSeconds: number, pauseMicroSeconds: number): void {
                 pins.analogWritePin(this.pin, 511)
-                control.waitMicros(Math.max(1, markMicroSeconds + markTimingCorrectionMicroSeconds))
+                control.waitMicros(100)
                 pins.analogWritePin(this.pin, 0)
-                control.waitMicros(Math.max(1, pauseMicroSeconds + pauseTimingCorrectionMicroSeconds))
+                control.waitMicros(200)
             }
         }
 
