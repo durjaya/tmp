5e710c23286c6e70caaf0b71ad9e18b50e29d462
Jim Liu <1234959@qq.com>
2018-12-11 21:37:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -130,6 +130,45 @@ namespace powerfunctions {
           control.waitMicros(d * FACTOR);
     }
 
+       // Stop
+       function stop() {
+    
+        //1
+        ledOn(6000);
+        //2
+        ledOff(600);
+        //3
+        ledOn(600);
+        //4
+        ledOff(1500);
+        //5
+        ledOn(1500);
+        //6
+        ledOff(600);
+        //7
+        ledOn(1500);
+        //8
+        ledOff(600);
+        //9
+        ledOn(1500);
+        //10
+        ledOff(600);
+        //11
+        ledOn(600);
+        //12
+        ledOff(1500);
+        //13
+        ledOn(1500);
+        //14
+        ledOff(600);
+        //15
+        ledOn(1500);
+        //16
+        ledOff(600);
+        //17
+        ledOn(1500);
+    }
+
 
     /**
      * Set speed of a motor.
@@ -138,7 +177,7 @@ namespace powerfunctions {
     //% block="2set"
     //% weight=60
     export function setSpeed() {
-        let x = 1
+        stop()
     }
 
     namespace message {
