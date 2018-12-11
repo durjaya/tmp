2d288ecbfc5e1cbed2768b07e033ae10d6fab7b4
Jim Liu <1234959@qq.com>
2018-12-11 21:19:57 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -113,7 +113,63 @@ namespace powerfunctions {
         let x =1 
     }
 
-
+    function ledOn(d: number) {
+        let r = d * FACTOR;
+        while (r > 26) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            control.waitMicros(2);
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            r = r - 26;
+        }
+    }
+    
+    
+    
+    
+    function ledOff(d: number) {
+        value =d 
+        control.waitMicros(d * FACTOR);
+    }
+    
+    
+    // Stop
+    function stop() {
+    
+        //1
+        ledOn(6000);
+        //2
+        ledOff(600);
+        //3
+        ledOn(600);
+        //4
+        ledOff(1500);
+        //5
+        ledOn(1500);
+        //6
+        ledOff(600);
+        //7
+        ledOn(1500);
+        //8
+        ledOff(600);
+        //9
+        ledOn(1500);
+        //10
+        ledOff(600);
+        //11
+        ledOn(600);
+        //12
+        ledOff(1500);
+        //13
+        ledOn(1500);
+        //14
+        ledOff(600);
+        //15
+        ledOn(1500);
+        //16
+        ledOff(600);
+        //17
+        ledOn(1500);
+    }
     /**
      * Set speed of a motor.
      */
@@ -122,7 +178,7 @@ namespace powerfunctions {
     //% weight=60
     export function setSpeed() {
         let a = 1
-        test()
+        stop()
     }
 
     namespace message {
