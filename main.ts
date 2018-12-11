499f793e4c7f88c1dee7e10c04ea338e527fd239
Jim Liu <1234959@qq.com>
2018-12-11 21:44:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,6 @@
 //% weight=99 color=#0fbc11 icon="\uf0e4" block="Power Functions"
 namespace powerfunctions {
 
-    
-
-   
-
     let FACTOR = 1
 
     function ledOn(d: number) {
@@ -25,43 +21,7 @@ namespace powerfunctions {
           control.waitMicros(d * FACTOR);
     }
 
-    // Stop
-    function stop() {
-
-    //1
-    ledOn(6000);
-    //2
-    ledOff(600);
-    //3
-    ledOn(600);
-    //4
-    ledOff(1500);
-    //5
-    ledOn(1500);
-    //6
-    ledOff(600);
-    //7
-    ledOn(1500);
-    //8
-    ledOff(600);
-    //9
-    ledOn(1500);
-    //10
-    ledOff(600);
-    //11
-    ledOn(600);
-    //12
-    ledOff(1500);
-    //13
-    ledOn(1500);
-    //14
-    ledOff(600);
-    //15
-    ledOn(1500);
-    //16
-    ledOff(600);
-    //17
-    ledOn(1500);
+  
 }
 
 
@@ -72,10 +32,84 @@ namespace powerfunctions {
     //% block="2set"
     //% weight=60
     export function setSpeed() {
-        stop()
+        //1
+        ledOn(6000);
+        //2
+        ledOff(600);
+        //3
+        ledOn(600);
+        //4
+        ledOff(1500);
+        //5
+        ledOn(1500);
+        //6
+        ledOff(600);
+        //7
+        ledOn(1500);
+        //8
+        ledOff(600);
+        //9
+        ledOn(1500);
+        //10
+        ledOff(600);
+        //11
+        ledOn(600);
+        //12
+        ledOff(1500);
+        //13
+        ledOn(1500);
+        //14
+        ledOff(600);
+        //15
+        ledOn(1500);
+        //16
+        ledOff(600);
+        //17
+        ledOn(1500);
     }
 
-
+    /**
+     * Set speed of a motor.
+     */
+    //% blockId=xxx
+    //% block="3set"
+    //% weight=60
+    export function setStop() {
+        //1
+        ledOn(6000);
+        //2
+        ledOff(600);
+        //3
+        ledOn(600);
+        //4
+        ledOff(1500);
+        //5
+        ledOn(1500);
+        //6
+        ledOff(600);
+        //7
+        ledOn(1500);
+        //8
+        ledOff(600);
+        //9
+        ledOn(1500);
+        //10
+        ledOff(600);
+        //11
+        ledOn(600);
+        //12
+        ledOff(1500);
+        //13
+        ledOn(1500);
+        //14
+        ledOff(600);
+        //15
+        ledOn(1500);
+        //16
+        ledOff(600);
+        //17
+        ledOn(1500);
+    }
 
 
 
